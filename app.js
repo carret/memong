a7e0883d474181d89b53bc938f26f481cdc7f3a1
kjwook <cmdhema@gmail.com>
2015-08-19 00:09:24 +0900

    Fix including assets folder

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set('view engine', 'ejs');
 app.use(passports.initialize());
 app.use(passports.session());
 app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/assets'));
 
 routes.doRoutes(app);
 
